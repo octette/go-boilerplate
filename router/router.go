c87a26a47d897e3ea13b379e360358a9fbe92ecb
ufukomer <ufukomer@gmail.com>
2017-07-19 13:17:08 +0300

    GetUserList handler

--- router/router.go
+++ router/router.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/ufukomer/tagon-api/handler"
 )
 
 func Load(middleware ...gin.HandlerFunc) http.Handler {
@@ -12,6 +13,11 @@ func Load(middleware ...gin.HandlerFunc) http.Handler {
 	e.Use(gin.Recovery())
 	e.Use(gin.Logger())
 	e.Use(middleware...)
+
+	v3 := e.Group("/v3")
+	{
+		v3.GET("users", handler.GetUserList)
+	}
 
 	return e
 }
