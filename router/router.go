33065830ade87d0c16c58c2acc83dccd2768f3a2
ufukomer <ufukomer@gmail.com>
2017-07-19 12:41:06 +0300

    Router config

--- router/router.go
+++ router/router.go
@@ -1,1 +1,11 @@
 package router
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Load(middleware ...gin.HandlerFunc) http.Handler {
+
+}
--- store/store.go
+++ store/store.go
@@ -1,9 +1,1 @@
 package store
-
-import (
-	"github.com/gin-gonic/gin"
-)
-
-func Load(middleware ...gin.HandlerFunction) {
-
-}
