017855ec74c22ba0b5e39848905722405590ad57
ufukomer <ufukomer@gmail.com>
2017-07-26 18:32:45 +0300

    User methods

--- model/user.go
+++ model/user.go
@@ -1,9 +1,33 @@
 package model
 
-import "github.com/jinzhu/gorm"
+import (
+	"errors"
+	"regexp"
+
+	"github.com/jinzhu/gorm"
+)
+
+// validate a email
+var reEmail = regexp.MustCompile("^[a-z0-9._%+\\-]+@[a-z0-9.\\-]+\\.[a-z]{2,4}$")
+
+var errUserEmailInvalid = errors.New("Invalid User Email")
 
 type User struct {
 	gorm.Model
 	Email    string
 	Password string
+}
+
+// Validate validates the required fields and formats.
+func (u *User) Validate() error {
+	switch {
+	case len(u.Email) == 0:
+		return errUserEmailInvalid
+	case len(u.Email) > 250:
+		return errUserEmailInvalid
+	case !reEmail.MatchString(u.Email):
+		return errUserEmailInvalid
+	default:
+		return nil
+	}
 }
--- store/store.go
+++ store/store.go
@@ -9,6 +9,8 @@ import (
 type Store interface {
 	GetUserList() ([]*model.User, error)
 	GetUser(id uint) (*model.User, error)
+	CreateUser(user *model.User) error
+	DeleteUser(id uint) error
 }
 
 // GetUserList gets a list of all users in the system.
@@ -19,4 +21,12 @@ func GetUserList(c context.Context) ([]*model.User, error) {
 // GetUser gets the user object corresponding to the given id.
 func GetUser(c context.Context, id uint) (*model.User, error) {
 	return FromContext(c).GetUser(id)
+}
+
+func CreateUser(c context.Context, user *model.User) error {
+	return FromContext(c).CreateUser(user)
+}
+
+func DeleteUser(c context.Context, id uint) error {
+	return FromContext(c).DeleteUser(id)
 }
